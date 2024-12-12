f42b4d5754d228d5128293e8a7c2e5750289a416
Lucian Stan <lucian.stan@syneto.eu>
2024-12-12 13:22:12 +0200

    Feat: added a test for invalid order, invalid quantity

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -31,6 +31,13 @@ class TestOrders:
         assert response.status_code == 400
 
     @pytest.mark.asyncio
+    async def test_create_order_invalid_quantity(self, client, base_url):
+        url = f"{base_url}/orders/"
+        payload = {"stoks": "EURUSD", "quantity": "a_string"} # quantity is string
+        response = client.post(url, json=payload)
+        assert response.status_code == 400
+
+    @pytest.mark.asyncio
     async def test_get_specific_order(self, client, base_url):
         url = f"{base_url}/orders/"
         # Create order first
